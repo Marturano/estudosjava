88f0a1e9d0c8089aa2df62ed39d499c6fc424f00
Unknown <samuelbarbelli@hotmail.com>
2018-01-25 12:15:38 -0400

    2018.01.25 - 12.15 - samuel

--- javaeclipse/js1/src/js1/javinha5.java
+++ javaeclipse/js1/src/js1/javinha5.java
@@ -2,17 +2,35 @@ package js1;
 
 public class javinha5 {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 
 		int S, Y, SY;
 
 		S = 19;
 		Y = 19;
-		
+
 		SY = S + Y;
 
 		System.out.println("Vou fazer "+SY+" anos.");
 
 	}
+
+	public static int nome() {
+
+		//		 System.out.println("nome");
+
+		return 112313213;
+
+	}
+
+	public static int soma(int int1, int int2) {
+
+		//		 System.out.println("nome");
+
+		return int1+int2;
+
+	}
+
+
 
 }
---
+++ javaeclipse/js1/src/js2/js2.java
@@ -0,0 +1,21 @@
+package js2;
+
+import js1.javinha5;
+
+public class js2 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		javinha5 objavinha5 = new javinha5();
+		int retorno;
+//		retorno = objavinha5.nome();
+		
+		retorno = objavinha5.soma(4, 5);
+		
+		System.out.println(retorno);
+		
+	}
+	
+}
